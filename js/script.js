2fcafc1eb0376062fa6cc0f2045d6f3408d72bff
ChristopherS24 <chryskies@gmail.com>
2024-09-04 10:04:51 +0200

    sbarrati i true

--- js/script.js
+++ js/script.js
@@ -3,16 +3,72 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            todolist: [
-                'Alzarsi dal letto',
-                'lavarsi',
-                'fare colazione',
-                'studiare',
-                'pranzare',
-                'uscire con gli amici',
-                'cenare',
-                'andare a dormire non troppo tardi'
-            ]
+            // todolists: [
+            //     'Alzarsi dal letto',
+            //     'Lavarsi',
+            //     'Fare colazione',
+            //     'Studiare',
+            //     'Pranzare',
+            //     'Uscire con gli amici',
+            //     'Cenare',
+            //     'Guardare la televisione',
+            //     'Andare a dormire non troppo tardi'
+            // ],
+            todolists: [
+                {
+                    text: 'Alzarsi dal letto',
+                    done: true,
+                },
+                {
+                    text: 'Lavarsi',
+                    done: true,
+                },
+                {
+                    text: 'Fare colazione',
+                    done: false,
+                },
+                {
+                    text: 'Studiare',
+                    done: true,
+                },
+                {
+                    text: 'Pranzare',
+                    done: true,
+                },
+                {
+                    text: 'Uscire con gli amici',
+                    done: true,
+                },
+                {
+                    text: 'Cenare',
+                    done: true,
+                },
+                {
+                    text: 'Guardare la televisione',
+                    done: true,
+                },
+                {
+                    text: 'Andare a dormire non troppo tardi',
+                    done: false,
+                },
+            ],
+            newTodolist: ''
         };
+    },
+    methods: {
+        addTodolist() {
+            const cleanTodolist = this.newTodolist.trim();
+
+            if (cleanTodolist != '') {
+                this.todolists.push({
+                    text: cleanTodolist,
+                    done: false
+                });
+                this.newTodolist = '';
+            }
+        },
+        removeTodolist(i) {
+            this.todolists.splice(i, 1);
+        }
     }
-}).mount('#app')
+}).mount('#app');
